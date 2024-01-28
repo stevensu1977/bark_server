02770ee22a2d70c013acfde122070300c8533abe
Steven Su <suwei007@gmail.com>
2024-01-28 11:10:35 +0800

    add long form support
    Signed-off-by: Steven Su <suwei007@gmail.com>

--- server/api.py
+++ server/api.py
@@ -12,11 +12,62 @@ from pydantic import BaseModel
 import uvicorn
 import os
 
-host = os.getenv("HOST", "127.0.0.1")
+import scipy
+import nltk
+from bark import generate_audio, SAMPLE_RATE
+
+host = os.getenv("HOST", "0.0.0.0")
 port = os.getenv("PORT", "8000")
 
 port = int(port)
 
+SUPPORT_SPEAKERS=["v2/en_speaker_6","v2/en_speaker_9"]
+
+def predict_long_form(processor,model,txt,voice_preset=None):
+    sentences = nltk.sent_tokenize(txt.replace("\n", " ").strip())
+    SPEAKER = "v2/en_speaker_6"
+    if voice_preset in SUPPORT_SPEAKERS:
+        SPEAKER = voice_preset
+    
+    silence = np.zeros(int(0.25 * SAMPLE_RATE))  # quarter second of silence
+
+    print(f"{voice_preset=}")
+    pieces = []
+    for sentence in sentences:
+        audio_array = generate_audio(sentence, history_prompt=SPEAKER)
+        pieces += [audio_array, silence.copy()]
+
+   
+    #pieces = model.generate(**inputs, do_sample=True)
+    
+    # audio_array = np.concatenate([i.cpu().numpy()[:l] for i,l in zip(pieces[0],pieces[1])])
+    #scipy.io.wavfile.write("../outputs/example.wav",rate=SAMPLE_RATE, data=np.concatenate(pieces))
+
+    
+    # Now we need to move our tensors back to the CPU and convert them to a NumPy array
+    #speech = speech_values.cpu().numpy().squeeze()
+
+    # The soundfile library requires us to convert our array of float16 numbers
+    # into an array of int16 numbers.
+    #scaled_speech = np.int16(speech * 32767)
+
+    wav = BytesIO()
+    sf.write(wav, np.concatenate(pieces), SAMPLE_RATE, format="WAV", subtype="PCM_16")
+
+
+    # WAV files are huge, so we're going to use pydub to convert it to a much
+    # smaller mp3 "file"
+    audio = AudioSegment.from_wav(wav)
+    dur_ms = len(audio)
+
+    mp3 = BytesIO()
+    audio.export(mp3, format="mp3")
+
+    return mp3, dur_ms
+    #return "",""
+    
+
+    
 
 def predict(processor, model, text, voice_preset=None):
     # We need to convert our text into something the model can understand,
@@ -82,7 +133,9 @@ async def health_check():
 @app.post("/generate", response_class=StreamingResponse)
 def generate(req: GenerateRequest = Body(...)):
     try:
-        mp3, dur_ms = predict(processor, model, req.text, req.voice_preset)
+        #mp3, dur_ms = predict(processor, model, req.text, req.voice_preset)
+        mp3, dur_ms = predict_long_form(processor,model, req.text, req.voice_preset)
+        print(f"Voice duration {dur_ms}/ms")
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
